cd2d19905f6b83d109e791f6a04f93e1e3283a67
intimanjunath <intimanjunath@gmail.com>
2026-01-28 10:00:00 -0800

    Add public random chat client

---
+++ public/js/sock.js
@@ -0,0 +1,65 @@
+var name = names;
+console.log(name);
+const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+};
+
+let cook = getCookie("jwt");
+console.log(cook);
+const socket = io("http://localhost:3000", {
+  query: {
+    token: cook,
+  },
+});
+let matchedname = undefined;
+const chatContainer = document.querySelector("#messages");
+const reqbutton = document.getElementById("sendrequest");
+
+// Connection Event Handler
+socket.on("connect", function () {
+  console.log("Connected to server");
+  socket.emit("joinRoom", {
+    name: name,
+  });
+});
+
+// Message Event Handler
+socket.on("message", function (message) {
+  console.log(message);
+  const newNode = document.createElement("div");
+  newNode.innerHTML = `<b>${message.name}:&nbsp;</b> ${message.text}`;
+  document.getElementById("messages").appendChild(newNode);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+});
+
+// User Left Event Handler
+socket.on("user_left", disableChatBox);
+
+function disableChatBox() {
+  document.querySelector(".butt2").disabled = true;
+  document.querySelector("textarea").disabled = true;
+}
+
+// Form Submission Event Handler
+
+let message_input = document.getElementById("message_input");
+
+document.getElementById("message_input_form").onsubmit = function (e) {
+  e.preventDefault();
+  let chattext = message_input.value.trim();
+  if (chattext.length) {
+    socket.emit("message", {
+      name: name,
+      text: chattext,
+    });
+  }
+  document.getElementById("message_input").value = "";
+};
+message_input.value = "";
+message_input.focus();
+
+reqbutton.addEventListener("click", (e) => {
+  e.preventDefault();
+});
